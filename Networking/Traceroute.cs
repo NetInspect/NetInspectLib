b93ebc0c3a294d5690d308a8df1be52ce46c9793
yatt-ze <arthurhuia@gmail.com>
2023-03-26 00:45:36 +1300

    Create Traceroute Class

---
+++ Networking/Traceroute.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Net;
+
+using NetInspectLib.Types;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace NetInspectLib.Networking
+{
+    public class Traceroute : IEnumerable
+    {
+        public Route route { get; }
+
+        public Traceroute()
+        {
+            route = new Route();
+        }
+
+        public bool DoTraceroute(string hostname, int maxHops)
+        {
+            IPAddress[] addresses;
+
+            try
+            {
+                addresses = Dns.GetHostAddresses(hostname);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error getting IP address for {hostname}: {ex.Message}");
+                return false;
+            }
+
+            if (addresses.Length == 0)
+            {
+                Debug.WriteLine($"No IP address found for {hostname}");
+                return false;
+            }
+
+            IPAddress ip = addresses[0];
+
+            for (int ttl = 1; ttl <= maxHops; ttl++)
+            {
+                PingOptions options = new PingOptions(ttl, true);
+                Ping ping = new Ping();
+
+                try
+                {
+                    PingReply reply = ping.Send(ip, 5000, new byte[32], options);
+
+                    if (reply.Status == IPStatus.TtlExpired || reply.Status == IPStatus.Success)
+                    {
+                        string address = reply.Address == null ? "*" : reply.Address.ToString();
+                        string time = reply.Status == IPStatus.Success ? reply.RoundtripTime + " ms" : "timeout";
+
+                        route.AddHop(address, time);
+
+                        if (reply.Status == IPStatus.Success)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        route.AddHop("*", "timeout");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Error pinging {ip}: {ex.Message}");
+                    route.AddHop("*", "timeout");
+                }
+            }
+
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var hop in route.hops)
+            {
+                yield return hop;
+            }
+        }
+    }
+}
