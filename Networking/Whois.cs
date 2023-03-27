c93b74dbe7f23b44564b78325feece8502b1dd87
Gumball2000 <tahana.perigo@gmail.com>
2023-03-28 11:37:28 +1300

    Added whois class

--- Networking/Whois.cs
+++ Networking/Whois.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using System.Net.Sockets;
+using System.IO;
+using System.Text;
 
-public class Class1
+public class Whois
 {
-	public Class1()
-	{
-	}
+    private const int DEFAULT_PORT = 43;
+    private const string DEFAULT_SERVER = "whois.iana.org";
+
+    public string DoWhoIsLookup(string query, string server = DEFAULT_SERVER, int port = DEFAULT_PORT)
+    {
+        string result = "";
+        using (TcpClient client = new TcpClient(server, port))
+        {
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] queryBytes = Encoding.ASCII.GetBytes(query + "\r\n");
+                stream.Write(queryBytes, 0, queryBytes.Length);
+
+                using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+        }
+
+        return result;
+    }
 }
