864ea1d58809aaed3bf9169f5b1aa29ac876b3bb
Gumball2000 <tahana.perigo@gmail.com>
2023-03-28 09:16:27 +1300

    Added other record names and got rid of TTL.

--- Networking/DNSScan.cs
+++ Networking/DNSScan.cs
@@ -1,55 +1,52 @@
 using System.Net.Sockets;
 using System.Net;
+using NetInspectLib.Types;
 using System.Collections.Generic;
 
 public class DNSScanner
 {
-    public List<DNS> Scan(string target)
+    public List<DNS> DoDNSScan(string target)
     {
         var records = new List<DNS>();
-
-        // Lookup hostname and add A records
         var hostEntry = Dns.GetHostEntry(target);
+
         foreach (var address in hostEntry.AddressList)
         {
             records.Add(new DNS
             {
-                Hostname = target,
-                IPAddress = address.ToString(),
+                Host = new Host(address.ToString(), target),
                 RecordType = "A",
-                TTL = (int)hostEntry.AddressList[0].AddressFamily,
                 Data = null,
                 CName = hostEntry.HostName,
                 NameServer = Dns.GetHostEntry(target).HostName
             });
+
         }
 
-        // Lookup other records (CNAME, MX, TXT)
-        foreach (var recordType in new string[] { "CNAME", "MX", "TXT" })
+        foreach (var recordType in new string[] { "CNAME", "MX", "TXT", "AAAA", "NS", "SOA", "PTR", "DNSKEY", "DS", "NAPTR" })
         {
             try
             {
-                var DNSs = Dns.GetHostEntry(target + "." + recordType);
+                var DNSs = Dns.GetHostEntry($"{target}.{recordType}");
+
                 foreach (var DNS in DNSs.AddressList)
                 {
                     records.Add(new DNS
                     {
-                        Hostname = target,
-                        IPAddress = DNS.ToString(),
+                        Host = new Host(DNS.ToString(), target),
                         RecordType = recordType,
-                        TTL = (int)DNSs.AddressList[0].AddressFamily,
                         Data = null,
                         CName = hostEntry.HostName,
                         NameServer = Dns.GetHostEntry(target).HostName
                     });
                 }
             }
+
             catch (SocketException ex)
             {
-                // Ignore exceptions caused by records that don't exist
                 if (ex.SocketErrorCode != SocketError.HostNotFound && ex.SocketErrorCode != SocketError.NoData)
                 {
-                    throw;
+                    continue;
                 }
             }
         }
