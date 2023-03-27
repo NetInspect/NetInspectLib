818231ae5a02d0f4ad7333639a287a3a174e0829
Gumball2000 <tahana.perigo@gmail.com>
2023-03-28 09:24:17 +1300

    Improvements

--- Types/DNS.cs
+++ Types/DNS.cs
@@ -1,10 +1,10 @@
 using NetInspectLib.Types;
+using System.Collections.Generic;
 
 public class DNS
 {
-    public string Hostname { get; set; }
-    public string IPAddress { get; set; }
-    public string RecordType { get; set; }
+    public Host? Host { get; set; }
+    public string? RecordType { get; set; }
     public int TTL { get; set; }
     public List<string>? Data { get; set; }
     public string? CName { get; set; }
