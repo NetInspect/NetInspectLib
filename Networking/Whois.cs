8d18c085e36bb6d3dc0d80fa05dfd221af260583
Gumball2000 <89838170+Gumball2000@users.noreply.github.com>
2023-05-02 16:44:10 +1200

    Update Whois.cs

--- Networking/Whois.cs
+++ Networking/Whois.cs
@@ -1,5 +1,6 @@
-﻿using System.Net.Sockets;
+using System.Net.Sockets;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Text;
 
 public class Whois
@@ -7,23 +8,40 @@ public class Whois
     private const int DEFAULT_PORT = 43;
     private const string DEFAULT_SERVER = "whois.iana.org";
 
-    public string DoWhoIsLookup(string query, string server = DEFAULT_SERVER, int port = DEFAULT_PORT)
+    public string Lookup(string query, string server = DEFAULT_SERVER, int port = DEFAULT_PORT)
     {
         string result = "";
-        using (TcpClient client = new TcpClient(server, port))
-        {
-            using (NetworkStream stream = client.GetStream())
-            {
-                byte[] queryBytes = Encoding.ASCII.GetBytes(query + "\r\n");
-                stream.Write(queryBytes, 0, queryBytes.Length);
 
-                using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
+        using (TcpClient client = new TcpClient())
+        {
+            client.Connect(server, port);
+
+            using (NetworkStream stream = client.GetStream())
+            using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
+            using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
+            {
+                writer.WriteLine(query);
+                writer.Flush();
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    result = reader.ReadToEnd();
+                    result += line + "\n";
+                    if (line.StartsWith("refer:", System.StringComparison.OrdinalIgnoreCase))
+                    {
+                        // If the WHOIS server returns a "refer" response, follow the new server and query again
+                        string newServer = line.Substring(6).Trim();
+                        result += Lookup(query, newServer, port);
+                        break;
+                    }
                 }
             }
         }
 
-        return result;
+        // Remove comments and blank lines from the response
+        result = Regex.Replace(result, "^#.*", "", RegexOptions.Multiline);
+        result = Regex.Replace(result, "^\\s*$\n", "", RegexOptions.Multiline);
+
+        return result.Trim();
     }
 }
