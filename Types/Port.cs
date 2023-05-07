7e43677e92b02cba9a3e02d7aa8de6c0b3fe05f0
Gumball2000 <tahana.perigo@gmail.com>
2023-05-08 11:40:21 +1200

    Syntax error ","

--- Types/Port.cs
+++ Types/Port.cs
@@ -10,7 +10,7 @@
             { 20, "FTP Data" }, { 21, "FTP Control" },
             { 22, "SSH" },      { 23, "Telnet" },
             { 25, "SMTP" },     { 43, "whois" },
-            { 53, "DNS" },      { 68, "DHCP" }
+            { 53, "DNS" },      { 68, "DHCP" },
             { 80, "HTTP" },     { 110, "POP" },
             { 115, "SFTP" },      { 119, "NNTP" },
             { 123, "NTP" },     { 143, "IMAP" },
