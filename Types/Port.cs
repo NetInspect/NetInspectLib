72ecaedb6b5a9b6bb9c4e841fa72d22f16fc64c3
Gumball2000 <tahana.perigo@gmail.com>
2023-05-07 18:43:29 +1200

    Added more known-ports

--- Types/Port.cs
+++ Types/Port.cs
@@ -5,15 +5,20 @@
         public int Number { get; }
         public string Name { get; }
 
-        private Dictionary<int, string> knownPorts = new Dictionary<int, string>()
+        private static readonly Dictionary<int, string> knownPorts = new Dictionary<int, string>()
         {
             { 20, "FTP Data" }, { 21, "FTP Control" },
             { 22, "SSH" },      { 23, "Telnet" },
-            { 25, "SMTP" },     { 53, "DNS" },
+            { 25, "SMTP" },     { 43, "whois" },
+            { 53, "DNS" },      { 68, "DHCP" },
             { 80, "HTTP" },     { 110, "POP" },
+            { 115, "SFTP" },      { 119, "NNTP" },
             { 123, "NTP" },     { 143, "IMAP" },
-            { 443, "HTTPS" },   { 613, "IPP" },
-            { 3306, "MySQL" },  {5351, "NAT-PMP"}
+            { 161, "SNMP" },      { 220, "IMAP3" },
+            { 389, "LDAP" },      { 443, "HTTPS" },
+            { 445, "SMB" },   { 613, "IPP" },
+            { 3306, "MySQL" },  {5351, "NAT-PMP"} 
+          
         };
 
         public Port(int number, string? name = null)
