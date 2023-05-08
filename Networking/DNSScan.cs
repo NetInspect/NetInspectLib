037961287c3e5328e42b2d0a07efdd304973162f
Gumball2000 <tahana.perigo@gmail.com>
2023-05-09 11:34:12 +1200

    Added DNS XML documentation

--- Networking/DNSScan.cs
+++ Networking/DNSScan.cs
@@ -13,10 +13,29 @@ public class DnsRecord
     public string? Data { get; set; }
 }
 
+/// <summary>
+/// Provides methods to perform DNS lookups and retrieve DNS records.
+/// /// <example>
+/// Usage
+/// <code>
+///     DnsLookup dnsLookup = new DnsLookup();
+///     List<DnsRecord> results = dnsLookup.DoDNSLookup("google.com", QueryType.ANY);
+///     foreach(DnsRecord result in results)
+///     {
+///             //Do Something
+///     }
+/// </code>
+/// </example>
+/// </summary>
 public class DnsLookup
 {
     private readonly LookupClient _dnsClient;
 
+    /// <summary>
+    /// Initializes a new instance of the DnsLookup class with the specified DNS server.
+    /// If no DNS server is specified, a default one is used.
+    /// </summary>
+    /// <param name="dnsServer">The IP address of the DNS server to use.</param>
     public DnsLookup(string? dnsServer = null)
     {
         if (string.IsNullOrEmpty(dnsServer))
@@ -28,7 +47,12 @@ public class DnsLookup
             _dnsClient = new LookupClient(IPAddress.Parse(dnsServer));
         }
     }
-
+    /// <summary>
+    /// Looksup a domain or host name for DNS information with a given DNS server.
+    /// </summary>
+    /// <param name="domain_or_ip">The domain name or host IP for a reverse lookup (e.g "Google.com").</param>
+    /// <param name="queryType">The DNS record to query (e.g "A" for A records) if left empty or null the default is a "ANY" query. Note some DNS servers don't except "ANY" queries.</param>
+    /// <returns>A list of DNS records</returns>
     public List<DnsRecord> DoDNSLookup(string domain_or_ip, QueryType queryType = QueryType.ANY)
     {
         List<DnsRecord> results = new List<DnsRecord>();
