bb3e90e8736a4175f4e6d8f0857b26fb076e1b93
Gumball2000 <tahana.perigo@gmail.com>
2023-05-09 11:45:25 +1200

    Added in namespace and some more documentation

--- Networking/DNSScan.cs
+++ Networking/DNSScan.cs
@@ -4,90 +4,98 @@ using System.Net;
 using DnsClient;
 using DnsClient.Protocol;
 
-public class DnsRecord
-{
-    public string? DomainName { get; set; }
-    public string? RecordClass { get; set; }
-    public string? RecordType { get; set; }
-    public string? TimeToLive { get; set; }
-    public string? Data { get; set; }
-}
 
-/// <summary>
-/// Provides methods to perform DNS lookups and retrieve DNS records.
-/// /// <example>
-/// Usage
-/// <code>
-///     DnsLookup dnsLookup = new DnsLookup();
-///     List<DnsRecord> results = dnsLookup.DoDNSLookup("google.com", QueryType.ANY);
-///     foreach(DnsRecord result in results)
-///     {
-///             //Do Something
-///     }
-/// </code>
-/// </example>
-/// </summary>
-public class DnsLookup
+namespace NetInspectLib.Networking
 {
-    private readonly LookupClient _dnsClient;
 
     /// <summary>
-    /// Initializes a new instance of the DnsLookup class with the specified DNS server.
-    /// If no DNS server is specified, a default one is used.
+    /// Provides methods to perform DNS lookups and retrieve DNS records.
+    /// /// <example>
+    /// Usage
+    /// <code>
+    ///     DnsLookup dnsLookup = new DnsLookup();
+    ///     List<DnsRecord> results = dnsLookup.DoDNSLookup("google.com", QueryType.ANY);
+    ///     foreach(DnsRecord result in results)
+    ///     {
+    ///             //Do Something
+    ///     }
+    /// </code>
+    /// </example>
     /// </summary>
-    /// <param name="dnsServer">The IP address of the DNS server to use.</param>
-    public DnsLookup(string? dnsServer = null)
+    public class DnsLookup
     {
-        if (string.IsNullOrEmpty(dnsServer))
-        {
-            _dnsClient = new LookupClient();
-        }
-        else
-        {
-            _dnsClient = new LookupClient(IPAddress.Parse(dnsServer));
-        }
-    }
-    /// <summary>
-    /// Looksup a domain or host name for DNS information with a given DNS server.
-    /// </summary>
-    /// <param name="domain_or_ip">The domain name or host IP for a reverse lookup (e.g "Google.com").</param>
-    /// <param name="queryType">The DNS record to query (e.g "A" for A records) if left empty or null the default is a "ANY" query. Note some DNS servers don't except "ANY" queries.</param>
-    /// <returns>A list of DNS records</returns>
-    public List<DnsRecord> DoDNSLookup(string domain_or_ip, QueryType queryType = QueryType.ANY)
-    {
-        List<DnsRecord> results = new List<DnsRecord>();
-        try
-        {
-            IDnsQueryResponse response = _dnsClient.Query(domain_or_ip, queryType);
-            string queryTypeString = response.Questions[0].QuestionType.ToString();
+        private readonly LookupClient _dnsClient;
 
-            foreach (DnsResourceRecord record in response.Answers)
+        /// <summary>
+        /// Initializes a new instance of the DnsLookup class with the specified DNS server.
+        /// If no DNS server is specified, a default one is used.
+        /// </summary>
+        /// <param name="dnsServer">The IP address of the DNS server to use.</param>
+        public DnsLookup(string? dnsServer = null)
+        {
+            if (string.IsNullOrEmpty(dnsServer))
             {
-                string[] parts = record.ToString().Split(' ');
-                string data = parts[parts.Length - 1];
+                _dnsClient = new LookupClient();
+            }
+            else
+            {
+                _dnsClient = new LookupClient(IPAddress.Parse(dnsServer));
+            }
+        }
+
+        /// <summary>
+        /// Looksup a domain or host name for DNS information with a given DNS server.
+        /// </summary>
+        /// <param name="domain_or_ip">The domain name or host IP for a reverse lookup (e.g "Google.com").</param>
+        /// <param name="queryType">The DNS record to query (e.g "A" for A records) if left empty or null the default is a "ANY" query. Note some DNS servers don't except "ANY" queries.</param>
+        /// <returns>A list of DNS records</returns>
+        public List<DnsRecord> DoDNSLookup(string domain_or_ip, QueryType queryType = QueryType.ANY)
+        {
+            List<DnsRecord> results = new List<DnsRecord>();
+            try
+            {
+                IDnsQueryResponse response = _dnsClient.Query(domain_or_ip, queryType);
+                string queryTypeString = response.Questions[0].QuestionType.ToString();
+
+                foreach (DnsResourceRecord record in response.Answers)
+                {
+                    string[] parts = record.ToString().Split(' ');
+                    string data = parts[parts.Length - 1];
+                    DnsRecord dnsRecord = new DnsRecord
+                    {
+                        DomainName = record.DomainName,
+                        RecordClass = record.RecordClass.ToString(),
+                        RecordType = record.RecordType.ToString(),
+                        TimeToLive = record.TimeToLive.ToString(),
+                        Data = data
+                    };
+                    results.Add(dnsRecord);
+                }
+            }
+            catch (DnsResponseException ex)
+            {
                 DnsRecord dnsRecord = new DnsRecord
                 {
-                    DomainName = record.DomainName,
-                    RecordClass = record.RecordClass.ToString(),
-                    RecordType = record.RecordType.ToString(),
-                    TimeToLive = record.TimeToLive.ToString(),
-                    Data = data
+                    DomainName = domain_or_ip,
+                    RecordClass = "Query failed",
+                    RecordType = ex.Message,
+                    TimeToLive = TimeSpan.Zero.ToString(),
+                    Data = ""
                 };
                 results.Add(dnsRecord);
             }
+            return results;
         }
-        catch (DnsResponseException ex)
+        
+        public class DnsRecord
         {
-            DnsRecord dnsRecord = new DnsRecord
-            {
-                DomainName = domain_or_ip,
-                RecordClass = "Query failed",
-                RecordType = ex.Message,
-                TimeToLive = TimeSpan.Zero.ToString(),
-                Data = ""
-            };
-            results.Add(dnsRecord);
+            public string? DomainName { get; set; }
+            public string? RecordClass { get; set; }
+            public string? RecordType { get; set; }
+            public string? TimeToLive { get; set; }
+            public string? Data { get; set; }
         }
-        return results;
+
     }
+
 }
--- Networking/Whois.cs
+++ Networking/Whois.cs
@@ -3,43 +3,67 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Text;
 
-public class Whois
+
+namespace NetInspectLib.Networking
 {
-    private const int DEFAULT_PORT = 43;
-    private const string DEFAULT_SERVER = "whois.iana.org";
+    /// <summary>
+    /// Class for doing a WHOIS query
+    /// <example>
+    /// Usage
+    /// <code>
+    ///     Whois whois = new Whois();
+    ///     string result = whois.Lookup("Google.com");
+    ///     //Do something with result e.g Console.Writeline(result); 
+    /// </code>
+    /// </example>
+    /// </summary>
 
-    public string Lookup(string query, string server = DEFAULT_SERVER, int port = DEFAULT_PORT)
+    public class Whois
     {
-        string result = "";
+        private const int DEFAULT_PORT = 43;
+        private const string DEFAULT_SERVER = "whois.iana.org";
 
-        using (TcpClient client = new TcpClient())
+        /// <summary>
+        /// Does a query for a specfied input using IANA WHOIS directory. 
+        /// </summary>
+        /// <param name="query">The query to preform (e.g Google.com). </param>
+        /// <param name="server">The WHOIS server to get the results from. using "whois.iana.org"</param>
+        /// <param name="port">The port to connect to the WHOIS server. </param>
+        /// <returns>The results of the WHOIS query. </returns>
+        public string Lookup(string query, string server = DEFAULT_SERVER, int port = DEFAULT_PORT)
         {
-            client.Connect(server, port);
+            string result = "";
 
-            using (NetworkStream stream = client.GetStream())
-            using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
-            using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
+            using (TcpClient client = new TcpClient())
             {
-                writer.WriteLine(query);
-                writer.Flush();
+                client.Connect(server, port);
 
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                using (NetworkStream stream = client.GetStream())
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
+                using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
                 {
-                    result += line + "\n";
-                    if (line.StartsWith("refer:", System.StringComparison.OrdinalIgnoreCase))
+                    writer.WriteLine(query);
+                    writer.Flush();
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        string newServer = line.Substring(6).Trim();
-                        result += Lookup(query, newServer, port);
-                        break;
+                        result += line + "\n";
+                        if (line.StartsWith("refer:", System.StringComparison.OrdinalIgnoreCase))
+                        {
+                            string newServer = line.Substring(6).Trim();
+                            result += Lookup(query, newServer, port);
+                            break;
+                        }
                     }
                 }
             }
-        }
-        
-        result = Regex.Replace(result, "^#.*", "", RegexOptions.Multiline);
-        result = Regex.Replace(result, "^\\s*$\n", "", RegexOptions.Multiline);
 
-        return result.Trim();
+            result = Regex.Replace(result, "^#.*", "", RegexOptions.Multiline);
+            result = Regex.Replace(result, "^\\s*$\n", "", RegexOptions.Multiline);
+
+            return result.Trim();
+        }
     }
 }
+
